7067ad72ef5431bc12a55a05f7cbd9bea0087a28
sujanghi129-pixel <sujanghi129@gmail.com>
2026-04-29 11:24:27 +0200

    corrected some minor errors

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost";
-$dbname = "EduSync";
+$dbname = "Edusync";
 $user = "root";
 $password = "";
 
--- database/student.sql
+++ database/student.sql
@@ -12,8 +12,11 @@ CREATE TABLE Student (
     CONSTRAINT FK_Student_Class  FOREIGN KEY (ClassID) REFERENCES Class(ClassID)
 );
 
-INSERT INTO Student(1,"Rajan Acharya","2010-05-15",14,TRUE,1,1);
-INSERT INTO Student(2,"Sita Thapa","2012-08-20",12,TRUE,2,2);
-INSERT INTO Student(3,"Hari Sharma","2009-11-30",15,TRUE,3,3);
-INSERT INTO Student(4,"Gita Rai","2011-03-10",13,TRUE,4,4);
-INSERT INTO Student(5,"Bikash Karki","2013-01-25",11,TRUE,5,5);
+INSERT INTO Student 
+(StudentId, FullName, DateOfBirth, Age, IsEnrolled, GradeID, ClassID)
+VALUES
+(1, 'Rajan Acharya', '2010-05-15', 14, TRUE, 1, 1),
+(2, 'Sita Thapa', '2012-08-20', 12, TRUE, 2, 2),
+(3, 'Hari Sharma', '2009-11-30', 15, TRUE, 3, 3),
+(4, 'Gita Rai', '2011-03-10', 13, TRUE, 4, 4),
+(5, 'Bikash Karki', '2013-01-25', 11, TRUE, 5, 5);
