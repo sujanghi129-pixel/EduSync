6e1cba0e582b221b4b6d110042bf5d0c8012d962
susma <susmappppp13@gmail.com>
2026-04-29 10:03:42 +0200

    added testing data to student table

--- database/student.sql
+++ database/student.sql
@@ -12,3 +12,8 @@ CREATE TABLE tblStudent (
     CONSTRAINT FK_Student_Class  FOREIGN KEY (ClassID) REFERENCES tblClass(ClassID)
 );
 
+INSERT INTO tblStudent(1,"Rajan Acharya","2010-05-15",14,TRUE,1,1);
+INSERT INTO tblStudent(2,"Sita Thapa","2012-08-20",12,TRUE,2,2);
+INSERT INTO tblStudent(3,"Hari Sharma","2009-11-30",15,TRUE,3,3);
+INSERT INTO tblStudent(4,"Gita Rai","2011-03-10",13,TRUE,4,4);
+INSERT INTO tblStudent(5,"Bikash Karki","2013-01-25",11,TRUE,5,5);
