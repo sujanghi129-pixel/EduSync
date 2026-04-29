e8bb2ec891e64ef98f0ae277f51e3ce1fe1ae44d
sujanghi129-pixel <sujanghi129@gmail.com>
2026-04-29 09:45:33 +0200

    created a database connection file

---
+++ config/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$dbname = "EduSync";
+$user = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
